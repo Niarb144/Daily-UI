880246d9acdb6b01346f0109ae8c6aebbbd4b4e3
Ted <54852276+Niarb144@users.noreply.github.com>
2024-03-20 16:04:52 +0300

    Add files via upload

---
+++ card-checkout/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Payment from '../src/Components/payment/index'
+
+function App() {
+  return (
+    <div>
+      <Payment/>
+    </div>
+  );
+}
+
+export default App;
